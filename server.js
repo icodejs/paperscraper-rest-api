b32714db48ef27d7afabbb76fdb8646c069d2c36
Jay Esco <icodejs@gmail.com>
2012-07-10 12:59:38 +0100

    add save functionality via query stringi

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-var
-app     = require('express').createServer(),
-scraper = require('./lib/scraper'),
-webPage = require('./lib/webPage');
+var app     = require('express').createServer(),
+    url     = require('url'),
+    qs      = require('querystring'),
+    scraper = require('./lib/scraper'),
+    webPage = require('./lib/webPage');
 
 function getImageData(req, res) {
-  console.log('-----------------');
-
   scraper.scrape({
     res: res,
     req: req,
     pageUrl: req.params.url,
     query: req.query,
     callback: function(err, obj) {
-      if (err) {
+      if (err)
         throw Error(err);
-      }
 
       res.end(obj.output);
     }
@@ -23,19 +21,29 @@ function getImageData(req, res) {
 
 function saveWebPage(req, res) {
   // this data needs to come from a post /get request from the server
+
+  var
+  parsedUrl = url.parse(req.url, true),
+  pathname  = parsedUrl.pathname,
+  query     = parsedUrl.query,
+  category  = query.category,
+  _url      = query.url;
+
+  if (!category || !_url)
+    throw new Error('Jay Esco: One or more query string values are missing. Formate = "?category=test&url=http://www.icodejs.com"');
+
   webPage.create({
-    category: 'animals',
-    url: 'http://animals.com'
+    category: category,
+    url: _url
   }, function(err, page) {
-    res.end(page);
+    res.end(JSON.stringify(page));
   });
 }
 
 function loadWebPages(req, res) {
   webPage.getWebPages(function(err, webPages) {
-    if (err) {
+    if (err)
       throw err;
-    }
 
     res.end(JSON.stringify(webPages));
   });
@@ -47,6 +55,10 @@ app.get('/scrape/page/', function(req, res){
 
 app.get('/load-web-pages/', function(req, res){
   loadWebPages(req, res);
+});
+
+app.get('/save-web-pages/', function(req, res){
+  saveWebPage(req, res);
 });
 
 app.listen(5000);
