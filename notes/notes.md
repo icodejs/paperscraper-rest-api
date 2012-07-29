26d043ff7ad1670ca46bc84e9888b11ab30b0cae
icodejs <icodejs@gmail.com>
2012-07-29 18:28:23 +0100

    tweaked urls, added wallpaper image functionality, refactoring, linting

---
+++ notes/notes.md
@@ -0,0 +1,21 @@
+# Notes
+
+*Use this for functionality ideas: http://youtu.be/X3Se49aJb2c
+
+*Image manipulation with node-canvas: http://thechangelog.com/post/1489735759/node-canvas-render-and-stream-html5-canvas-using-node-js
+
+*Node-canvas: https://github.com/LearnBoost/node-canvas
+
+*Loading images via web sockets: http://www.davidpirek.com/blog/loading-images-via-websockets-in-nodejs
+
+## Image/Wallpaper database table
+*name
+*filename
+*imageSrc
+*origin_url
+*domain
+*size
+*dimensions
+*alt text
+*title text
+*file type
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var app     = require('express').createServer(),
-    url     = require('url'),
-    qs      = require('querystring'),
-    scraper = require('./lib/scraper'),
-    webPage = require('./lib/webPage');
+var
+app       = require('express').createServer(),
+url       = require('url'),
+qs        = require('querystring'),
+scraper   = require('./lib/scraper'),
+webPage   = require('./lib/webPage'),
+wallpaper = require('./lib/wallpaper');
 
-function getImageData(req, res) {
+function scrapeWebPage(req, res) {
   scraper.scrape({
     res: res,
     req: req,
@@ -21,6 +23,7 @@ function getImageData(req, res) {
 
 function saveWebPage(req, res) {
   // this data needs to come from a post /get request from the server
+  // e.g. http://localhost:5000/save/webpages/?category=test&url=http://www.test.com
 
   var
   parsedUrl = url.parse(req.url, true),
@@ -41,24 +44,65 @@ function saveWebPage(req, res) {
 }
 
 function loadWebPages(req, res) {
-  webPage.getWebPages(function(err, webPages) {
-    if (err)
-      throw err;
-
+  webPage.all(function(err, webPages) {
+    if (err) throw err;
     res.end(JSON.stringify(webPages));
   });
 }
 
-app.get('/scrape/page/', function(req, res){
-  getImageData(req, res);
+function saveWallpaper(req, res) {
+  // this data needs to come from a post /get request from the server
+  // e.g. http://localhost:5000/save/wallpaper/?category=test&url=http://www.test.com
+
+  var
+  parsedUrl = url.parse(req.url, true),
+  pathname  = parsedUrl.pathname,
+  query     = parsedUrl.query;
+
+  wallpaper.create({
+    name        : query.name || '',
+    filename    : query.filename || '',
+    Description : query.Description || '',
+    imageSrc    : query.imageSrc || '',
+    originUrl   : query.originUrl || '',
+    domain      : query.domain || '',
+    fileSizeKB  : query.fileSizeKB || '',
+    dimensions  : query.dimensions || '',
+    width       : query.width || '',
+    height      : query.height || '',
+    altText     : query.altText || '',
+    titleText   : query.titleText || '',
+    fileType    : query.fileType || ''
+  }, function(err, page) {
+    res.end(JSON.stringify(page));
+  });
+}
+
+function loadWallpapers(req, res) {
+  wallpaper.all(function(err, webPages) {
+    if (err) throw err;
+    res.end(JSON.stringify(webPages));
+  });
+}
+
+app.get('/scrape/webPage/', function(req, res){
+  scrapeWebPage(req, res);
 });
 
-app.get('/load-web-pages/', function(req, res){
+app.get('/load/webPages/', function(req, res){
   loadWebPages(req, res);
 });
 
-app.get('/save-web-pages/', function(req, res){
+app.get('/save/webPage/', function(req, res){
   saveWebPage(req, res);
+});
+
+app.get('/save/wallpaper/', function(req, res){
+  saveWallpaper(req, res);
+});
+
+app.get('/load/wallpapers/', function(req, res){
+  loadWallpapers(req, res);
 });
 
 app.listen(5000);
